a27336ab4e8fee2493e7f1baaec413c54be51b81
lengxuelianghan <1048316915@qq.com>
2020-10-30 17:18:03 +0800

    修改

--- src/main/java/com/xiaoyuanpe/controller/SchoolController.java
+++ src/main/java/com/xiaoyuanpe/controller/SchoolController.java
@@ -95,7 +95,7 @@ public class SchoolController {
             }
         }catch (Exception ee) {
             resultBean.setCode(1);
-            resultBean.setMsg(ee.toString()+"," + subject.getPrincipal());
+            resultBean.setMsg(ee.toString()+"," + subject.hasRole("student"));
         }
         return resultBean;
     }
