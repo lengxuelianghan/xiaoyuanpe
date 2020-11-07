fee4cd36bc2f692770d7c66fda55278be01e892b
lengxuelianghan <1048316915@qq.com>
2020-11-07 21:23:05 +0800

    修改

--- src/main/java/com/xiaoyuanpe/controller/SemesterController.java
+++ src/main/java/com/xiaoyuanpe/controller/SemesterController.java
@@ -3,6 +3,7 @@ package com.xiaoyuanpe.controller;
 import com.xiaoyuanpe.pojo.Semester;
 import com.xiaoyuanpe.pojo.SemesterEntry;
 import com.xiaoyuanpe.pojo.Student;
+import com.xiaoyuanpe.pojo.User;
 import com.xiaoyuanpe.services.CollegeService;
 import com.xiaoyuanpe.services.SemesterService;
 import com.xiaoyuanpe.services.StudentService;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
@@ -27,8 +29,10 @@ public class SemesterController {
     @Autowired
     private CollegeService collegeService;
 
-    @RequestMapping("/score/{schoolId}")
-    public ResultBean getScoreBySchool(@PathVariable Integer schoolId){
+    @RequestMapping("/scoreBySchool")
+    public ResultBean getScoreBySchool(HttpSession session){
+        User user = (User) session.getAttribute("user");
+        Integer schoolId = user.getSchoolId();
         ResultBean resultBean = new ResultBean();
         List<SemesterEntry> semesterEntries = new ArrayList<>();
         try {
@@ -58,6 +62,55 @@ public class SemesterController {
             resultBean.setCode(1);
         }
 
+        return resultBean;
+    }
+    @RequestMapping("/scoreByClass")
+    public ResultBean classScore(HttpSession session){
+        User user = (User) session.getAttribute("user");
+        ResultBean resultBean = new ResultBean();
+        Student student = this.studentService.findStudentByNumber(user.getUserNumber());
+        List<SemesterEntry> semesterEntries = new ArrayList<>();
+        try {
+            List<Semester> semesters = this.semesterService.findSemesterAll();
+            for (Semester semester: semesters){
+                Student s  =this.studentService.findStudentById(semester.getSudentId());
+                if (semester.getSchoolId()==user.getSchoolId()&&student.getClassesId()==semester.getSchoolId()){
+                    SemesterEntry semesterEntry = new SemesterEntry();
+                    semesterEntry.setId(semester.getId());
+                    semesterEntry.setName(s.getStudentName());
+                    semesterEntry.setCollegeId(collegeService.findCollegeById(semester.getCollegeId()).getCollegeName());
+                    semesterEntry.setScore(semester.getScore());
+                    semesterEntries.add(semesterEntry);
+                }
+            }
+            semesterEntries.sort(new Comparator<SemesterEntry>() {
+                @Override
+                public int compare(SemesterEntry o1, SemesterEntry o2) {
+                    return o2.getScore().compareTo(o1.getScore());
+                }
+            });
+            resultBean.setData(semesterEntries);
+            resultBean.setCode(0);
+
+        }catch (Exception e){
+            resultBean.setMsg("查询失败"+e.getMessage());
+            resultBean.setCode(1);
+        }
+        return resultBean;
+    }
+    @RequestMapping("/singleScore")
+    public ResultBean singleScore(HttpSession session){
+        User user = (User) session.getAttribute("user");
+        ResultBean resultBean = new ResultBean();
+        Student student = this.studentService.findStudentByNumber(user.getUserNumber());
+        try {
+            Semester semester = this.semesterService.findSemesterByStudent(student.getShcoolId(), student.getCollegeId(), student.getClassesId(), student.getId()).get(0);
+            resultBean.setData(semester);
+            resultBean.setCode(0);
+        }catch (Exception e){
+            resultBean.setMsg("查询失败"+e.getMessage());
+            resultBean.setCode(1);
+        }
         return resultBean;
     }
 }
