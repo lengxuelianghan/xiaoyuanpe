ab00ef0c87369a1a536a83482b0f18ee653a8999
lengxuelianghan <1048316915@qq.com>
2021-01-06 14:41:54 +0800

    修改

--- src/main/java/com/xiaoyuanpe/controller/SignInController.java
+++ src/main/java/com/xiaoyuanpe/controller/SignInController.java
@@ -448,14 +448,13 @@ public class SignInController {
         ResultBean resultBean = new ResultBean();
         User user = (User) session.getAttribute("user");
         Student student = this.studentService.findStudentByNumber(user.getUserNumber());
-        resultBean.setData("ASS");
         ScorePerWeek activity = new ScorePerWeek("活动",0.0f);
         ScorePerWeek game = new ScorePerWeek("比赛",0.0f);
         ScorePerWeek sport = new ScorePerWeek("运动",0.0f);
-        resultBean.setData("s俄式是");
         try {
             List<SinglePeopleInfo> singlePeopleInfos = new ArrayList<>();
             List<Signin> signins = this.signInService.findSigninAll();
+            resultBean.setData("s俄式是");
             for (Signin signin: signins){
                 if (this.studentService.findStudentById(signin.getStudentId()).getClassesId()==student.getClassesId()){
                     Date endTime = signin.getSignoutTime();
