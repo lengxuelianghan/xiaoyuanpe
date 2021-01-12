d39a0afd3a28f3795ff855175a1b7fbe5d9be7a0
lengxuelianghan <1048316915@qq.com>
2021-01-12 23:11:18 +0800

    修改

--- src/main/java/com/xiaoyuanpe/pojo/StudentInfo.java
+++ src/main/java/com/xiaoyuanpe/pojo/StudentInfo.java
@@ -4,16 +4,20 @@ public class StudentInfo {
     private Integer id;
     private String name;
     private String number;
+    private String password;
     private String sex;
+    private Integer age;
     private String phone;
     private String school;
     private String college;
     private String classes;
 
-    public StudentInfo(String name, String number, String sex, String phone) {
+    public StudentInfo(String name, String number, String password, String sex, Integer age, String phone) {
         this.name = name;
         this.number = number;
+        this.password = password;
         this.sex = sex;
+        this.age = age;
         this.phone = phone;
     }
 
@@ -43,12 +47,28 @@ public class StudentInfo {
         this.number = number;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getSex() {
         return sex;
     }
 
     public void setSex(String sex) {
         this.sex = sex;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
     }
 
     public String getPhone() {
