905e5a44eea88b4e3d7e097b0a67d52f156bfc03
lengxuelianghan <1048316915@qq.com>
2021-01-16 15:10:33 +0800

    修改

--- src/main/java/com/xiaoyuanpe/controller/DownloadController.java
+++ src/main/java/com/xiaoyuanpe/controller/DownloadController.java
@@ -3,6 +3,7 @@ package com.xiaoyuanpe.controller;
 import com.xiaoyuanpe.units.ResultBean;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,7 +27,7 @@ public class DownloadController {
         return upload.getAbsolutePath();
     }
 
-    @GetMapping(value = "/downloadExcelModel", produces = "application/json;charset=UTF-8")
+    @PostMapping(value = "/downloadExcelModel")
     public ResultBean downloadExcelModel(HttpServletResponse response) {
         ResultBean resultBean = new ResultBean();
         String fileName = getUploadPath()+"/模板"+".xls";
