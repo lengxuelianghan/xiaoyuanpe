4aa9077d44557cab360a1fd2dc6106b5a8318db6
lengxuelianghan <1048316915@qq.com>
2021-03-18 16:32:11 +0800

    修改

--- src/main/java/com/xiaoyuanpe/XiaoyuanpeApplication.java
+++ src/main/java/com/xiaoyuanpe/XiaoyuanpeApplication.java
@@ -1,8 +1,15 @@
 package com.xiaoyuanpe;
 
+import com.xiaoyuanpe.pojo.User;
+import com.xiaoyuanpe.services.UserService;
+import com.xiaoyuanpe.units.SpringUtil;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.thymeleaf.spring5.context.SpringContextUtils;
+
+import java.util.List;
 
 @SpringBootApplication
 @MapperScan("com.xiaoyuanpe.mapper")//指定扫描接口与配置文件的包名
@@ -10,6 +17,18 @@ public class XiaoyuanpeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(XiaoyuanpeApplication.class, args);
+        ApplicationContext context = SpringUtil.getApplicationContext();
+        UserService userService = context.getBean(UserService.class);// 注意是UserServiceI ， 不是UserServiceImpl
+        List<User> usersListAll = userService.findUsersListAll();
+        if (usersListAll==null||usersListAll.size()==0){
+            User user = new User();
+            user.setIdentity("网站管理员");
+            user.setUsername("管理员");
+            user.setUnit("无");
+            user.setUserNumber("123456");
+            user.setPassword("123456");
+            userService.addUser(user);
+        }
     }
 
 }
