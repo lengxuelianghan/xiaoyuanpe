529fff26325d02c996dd8941ecdebf9d9a0e60f6
lengxuelianghan <1048316915@qq.com>
2021-03-20 17:31:40 +0800

    修改

--- src/main/java/com/xiaoyuanpe/services/SchoolServiceImpl.java
+++ src/main/java/com/xiaoyuanpe/services/SchoolServiceImpl.java
@@ -3,21 +3,41 @@ package com.xiaoyuanpe.services;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.xiaoyuanpe.mapper.SchoolMapper;
-import com.xiaoyuanpe.pojo.Page;
-import com.xiaoyuanpe.pojo.School;
-import com.xiaoyuanpe.pojo.SchoolExample;
+import com.xiaoyuanpe.pojo.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 @Service
 public class SchoolServiceImpl implements SchoolService {
-    @Autowired
+    @Resource
     private SchoolMapper schoolMapper;
+    @Resource
+    private UserService userService;
+    @Resource
+    private StudentService studentService;
     @Override
     public void addSchool(School school) {
         this.schoolMapper.insert(school);
+        if (school.getId()!=0){
+            User user = new User();
+            user.setSchoolId(school.getId());
+            user.setUnit(school.getSchoolName());
+            user.setUsername(""+school.getId()+(school.getSchoolNumber()==null?"1111111111":school.getSchoolNumber()));
+            user.setIdentity("学校管理员");
+            user.setPassword("123456");
+            user.setUnit(school.getSchoolName());
+            this.userService.addUser(user);
+            Student student = new Student();
+            student.setTerm(1);
+            student.setSchoolName(school.getSchoolName());
+            student.setStudentNumber(user.getUserNumber());
+            student.setSchoolId(school.getId());
+            student.setAddress(school.getSchoolAddress());
+            this.studentService.addStudent(student);
+        }
     }
 
     @Override
