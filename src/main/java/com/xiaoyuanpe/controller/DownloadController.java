0875dcdd7d0c91e009a66553c1f954ddcf56da2b
lengxuelianghan <1048316915@qq.com>
2021-03-25 15:46:33 +0800

    修改

--- src/main/java/com/xiaoyuanpe/controller/DownloadController.java
+++ src/main/java/com/xiaoyuanpe/controller/DownloadController.java
@@ -24,7 +24,6 @@ public class DownloadController {
         return upload.getAbsolutePath();
     }
 
-    @ResponseBody
     @GetMapping(value = "/downloadExcelModel/{num}")
     public ResultBean downloadExcelModel(@PathVariable Integer num, HttpServletResponse response) throws UnsupportedEncodingException {
         ResultBean resultBean = new ResultBean();
