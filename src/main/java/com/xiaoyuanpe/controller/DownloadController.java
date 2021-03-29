92086c0e8963b105828a8ea72e96f6f1fec44b3f
91074 <910749592@qq.com>
2021-03-29 14:28:32 +0800

    修改了下载文件方法

--- src/main/java/com/xiaoyuanpe/controller/DownloadController.java
+++ src/main/java/com/xiaoyuanpe/controller/DownloadController.java
@@ -46,11 +46,8 @@ public class DownloadController {
                 newFileName = "模板.xls";
                 break;
         }
-        /// 测试前端选择另存为路径
-//        response.setContentType("application/force-download");
-//        response.addHeader("Content-Disposition", "attachment; filename=" + new String(newFileName.getBytes("utf-8"),"iso-8859-1"));
         response.setContentType("application/octet-stream;charset=UTF-8");
-        response.addHeader("Content-Disposition", "attachment; filename=" + java.net.URLEncoder.encode(fileName, "UTF-8"));
+        response.addHeader("Content-Disposition", "attachment; filename=" + java.net.URLEncoder.encode(newFileName, "UTF-8"));
         File file = new File(fileName);
         FileInputStream fis = new FileInputStream(file);
         BufferedInputStream bis = new BufferedInputStream(fis);
@@ -65,54 +62,5 @@ public class DownloadController {
         }
         bis.close();
         bos.close();
-//
-////        response.setContentType("application/force-download");
-//        byte[] buff = new byte[1024];
-//        BufferedInputStream bis = null;
-//        OutputStream outputStream = null;
-//        FileInputStream inputStream = null;
-//        try {
-//            outputStream = response.getOutputStream();
-//            File file = new File(fileName);
-//            inputStream = new FileInputStream(file);
-//            bis = new BufferedInputStream(inputStream);
-//            int read = bis.read(buff);
-//
-//            while (read != -1) {
-//                outputStream.write(buff, 0, buff.length);
-//                outputStream.flush();
-//                read = bis.read(buff);
-//            }
-//
-////            resultBean.setCode(0);
-//        }catch (IOException  e){
-//            e.printStackTrace();
-////            resultBean.setCode(1);
-////            resultBean.setMsg("下载失败");
-//        }
-//        finally {
-//            if (bis != null) {
-//                try {
-//                    bis.close();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//            if (outputStream != null) {
-//                try {
-//                    outputStream.close();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//            if (bis!=null){
-//                try {
-//                    bis.close();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }
-//        return resultBean;
     }
 }
