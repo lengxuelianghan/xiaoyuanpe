a2686d2fbb4aa8c642f8fba557098c72c53e0dba
91074 <910749592@qq.com>
2021-03-29 13:46:19 +0800

    修改了下载文件方法

--- src/main/java/com/xiaoyuanpe/controller/DownloadController.java
+++ src/main/java/com/xiaoyuanpe/controller/DownloadController.java
@@ -25,7 +25,7 @@ public class DownloadController {
     }
 
     @RequestMapping(value = "/downloadFile/{type}")
-    public void downloadExcelModel(@PathVariable String type, HttpServletResponse response) throws UnsupportedEncodingException {
+    public void downloadExcelModel(@PathVariable String type, HttpServletResponse response) throws IOException {
         String fileName = "";
         String newFileName= null;
         switch (type) {
@@ -46,59 +46,18 @@ public class DownloadController {
                 newFileName = "模板.xls";
                 break;
         }
-        /// 测试前端选择另存为路径
-//        response.setContentType("application/force-download");
-//        response.addHeader("Content-Disposition", "attachment; filename=" + new String(newFileName.getBytes("utf-8"),"iso-8859-1"));
         response.setContentType("application/octet-stream;charset=UTF-8");
         response.addHeader("Content-Disposition", "attachment; filename=" + java.net.URLEncoder.encode(fileName, "UTF-8"));
-
-//        response.setContentType("application/force-download");
-        byte[] buff = new byte[1024];
-        BufferedInputStream bis = null;
-        OutputStream outputStream = null;
-        FileInputStream inputStream = null;
-        try {
-            outputStream = response.getOutputStream();
-            File file = new File(fileName);
-            inputStream = new FileInputStream(file);
-            bis = new BufferedInputStream(inputStream);
-            int read = bis.read(buff);
-
-            while (read != -1) {
-                outputStream.write(buff, 0, buff.length);
-                outputStream.flush();
-                read = bis.read(buff);
-            }
-
-//            resultBean.setCode(0);
-        }catch (IOException  e){
-            e.printStackTrace();
-//            resultBean.setCode(1);
-//            resultBean.setMsg("下载失败");
+        File file = new File(fileName);
+        FileInputStream inputStream = new FileInputStream(file);
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+        byte[] buffer = new byte[4096];
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(response.getOutputStream());
+        int offset = 0;
+        while (inputStream.read(buffer, offset, buffer.length) > -1) {
+            bufferedOutputStream.write(buffer,0,buffer.length);
         }
-        finally {
-            if (bis != null) {
-                try {
-                    bis.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (outputStream != null) {
-                try {
-                    outputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (bis!=null){
-                try {
-                    bis.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-//        return resultBean;
+        bufferedInputStream.close();
+        bufferedOutputStream.close();
     }
 }
