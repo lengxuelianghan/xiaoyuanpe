7d8b6cedd58b092d647952c95f8044266cb975d9
lengxuelianghan <1048316915@qq.com>
2021-04-01 17:35:59 +0800

    修改

--- src/main/java/com/xiaoyuanpe/units/ExcelUtilCollege.java
+++ src/main/java/com/xiaoyuanpe/units/ExcelUtilCollege.java
@@ -40,7 +40,13 @@ public class ExcelUtilCollege {
                             continue;
                         }
                         College college1 = new College();
-                        college1.setCollegeNumber(ExcelUtilCollege.getCellValue(sheet.getRow(j).getCell(0)));
+
+                        try {
+                            Double num = Double.valueOf(ExcelUtilCollege.getCell(sheet.getRow(j).getCell(0)));
+                            college1.setCollegeNumber(String.valueOf(num.intValue()));
+                        } catch (Exception e) {
+                            college1.setCollegeNumber(ExcelUtilCollege.getCell(sheet.getRow(j).getCell(0)));
+                        }
                         college1.setCollegeName(ExcelUtilCollege.getCell(sheet.getRow(j).getCell(1)));
                         college1.setScore(Integer.parseInt(ExcelUtilCollege.getCell(sheet.getRow(j).getCell(2))));
                         college1.setCollegeInfo(ExcelUtilCollege.getCell(sheet.getRow(j).getCell(3)));
